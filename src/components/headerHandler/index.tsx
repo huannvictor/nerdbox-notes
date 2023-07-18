2a2fd74b186e5a7caf288d91ae1ec898bc8fbbf4
Huann Almeida <huannvictor@gmail.com>
2023-07-18 13:40:30 -0300

    implementado useEffect()

--- src/components/headerHandler/index.tsx
+++ src/components/headerHandler/index.tsx
@@ -3,11 +3,16 @@
 import Header from '@/components/header'
 import HeaderLogged from '@/components/headerLogged'
 import { checkUserAuthenticated } from '@/services/isUserAuthenticated'
+import { useEffect, useState } from 'react'
 
 export default function HeaderHandler() {
-  const isUserAuth = checkUserAuthenticated()
+  const userAuth = checkUserAuthenticated()
+  const [isUserAuth, setIsUserAuth] = useState(userAuth)
+
+  useEffect(() => {
+    setIsUserAuth(userAuth)
+  }, [userAuth])
 
   if (isUserAuth) return <HeaderLogged />
-
   return <Header />
 }
--- src/components/loginButton/index.tsx
+++ src/components/loginButton/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { checkUserAuthenticated } from '@/services/isUserAuthenticated'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import SignInIcon from '../icons/SignInIcon'
@@ -11,11 +10,10 @@ interface Props {
 }
 
 const LoginButton: React.FC<Props> = ({ className, onClick }) => {
-  const isUserAuth = checkUserAuthenticated()
   const { push } = useRouter()
 
   const handleRouterPush = () => {
-    !isUserAuth ? push('/login') : push('/notes')
+    push('/login')
   }
 
   return (
