7541c267ccefca9ee40ea20bc634012ad93932ff
Huann Almeida <huannvictor@gmail.com>
2023-09-06 13:03:29 -0300

    componente para deletar usuário finalizado

--- src/components/users/deleteAccount/index.tsx
+++ src/components/users/deleteAccount/index.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import UsersService from '@/services/users'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import styles from '../UserForm.module.css'
+import UsersService from '@/services/users'
 
 const {
   field,
   title,
+  btnContainer,
+  btn,
   btnDelete
 } = styles
 
 export default function DeleteAccount() {
+  const [confirmDelete, setConfirmDelete] = useState(false)
   const { push } = useRouter()
 
   const handleDelete = async () => {
@@ -18,13 +22,29 @@ export default function DeleteAccount() {
     push('/')
   }
 
+  const handleConfirmDelete = () => {
+    setConfirmDelete(true)
+  }
+
+
 
   return (
     <fieldset className={field}>
       <span className={title}>Deletar sua conta:</span>
-      <button className={btnDelete} onClick={handleDelete}>
+      <button className={btnDelete} onClick={handleConfirmDelete}>
         Deletar
       </button>
+      {
+        confirmDelete 
+        ? (<div className='absolute bg-red-400 flex flex-col justify-center items-center w-fit p-3 z-50 rounded self-center'>
+            <span>Tem certeza disso?</span>
+            <div className={btnContainer}>
+              <button className={btnDelete} onClick={handleDelete}>Deletar</button>
+              <button className={btn}>Manter Conta</button>
+            </div>
+          </div>)
+        : null
+      } 
     </fieldset>
   )
 }
